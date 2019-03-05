206de142305e315203663b0572a43d3737740494
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-03-04 19:56:01 -0600

    if example

--- js-strings-objects/main.js
+++ js-strings-objects/main.js
@@ -1,1 +1,18 @@
-console.log(Jeressia);
+const iAmThursty = (num) => {
+    if (num < 21) {
+        return 'drink some water';
+    } else if (num > 20 && num < 65){
+        return 'drink a beer';
+    } else {
+        return 'take a nap';
+    }
+}
+
+const printToDom = (divId, textToPrint) => {
+     const selectedDiv=document.getElementById(divId);
+     selectedDiv.innerHTML += textToPrint;
+}
+
+printToDom('thirsty',iAmThursty(55));
+printToDom('thirsty',iAmThursty(7));
+printToDom('thirsty',iAmThursty(90));
